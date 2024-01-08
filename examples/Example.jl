9d77b80ea60c228626781b45a4b24b4e30cacd08
Michael Creel <michael.creel@uab.es>
2024-01-08 13:46:05 +0100

    training and mcmc working

--- examples/Example.jl
+++ examples/Example.jl
@@ -16,8 +16,8 @@ rmse_conv(Ŷ, Y) = mean(sqrt.(mean(abs2.(Ŷ - Y), dims=2)))
 function make_net(dev)
     # Set up the hyperparameters
     hp = HyperParameters(
-        validation_size=2_000, loss=rmse_conv, 
-        print_every=10, nsamples=100, epochs=1, dev=dev, batchsize=128
+        validation_size=5_000, loss=rmse_conv, 
+        print_every=10, nsamples=10000, epochs=2, dev=dev, batchsize=256
     )
     # Create the net
     net = MomentNetwork(
@@ -27,9 +27,9 @@ end
 
 # Create and train the net, and save final state
 function train_net(dgp)
-    net = make_net(cpu)
+    net = make_net(gpu)
     train_network!(net, dgp)
-    net_state = Flux.state(net)
+    net_state = cpu(Flux.state(net.best_model))
     BSON.@save "net_state.bson"  net_state
 end
 
@@ -38,25 +38,37 @@ function load_trained()
     isfile("net_state.bson") ? @info("loading trained net") : @info("load_trained: can't create net on CPU: net_state has not been saved")
     net = make_net(cpu) # recreate the net
     BSON.@load "net_state.bson"  net_state # get the trained parameters
-    Flux.loadmodel!(net, net_state)
-    Flux.testmode!(net)
+    Flux.loadmodel!(net.best_model, net_state)
+    Flux.testmode!(net.best_model)
     return net
 end
     
 
 # train_net(dgp)  # comment out when net is trained
 
-
 net = load_trained()
-
 data, θtrue = generate(dgp, net, 1)
 θnn = net.best_model(data)[:]
-
+@info "θtrue: ", θtrue
+@info "θnn:", θnn
 # set up proposal
 covreps = 1000
 _,Σₚ = simmomentscov(net, dgp, covreps, θnn)
 δ = 1.0 # tuning
 
 # do MCMC
-S = 50 
-chain = mcmc(θnn, θnn, δ, Σₚ, S, net, dgp) 
+S = 40 # simulations to compute moments
+# initial short chain
+chain = mcmc(θnn, θnn, δ, Σₚ, S, net, dgp; chainlength=500)
+accept = mean(chain[:,end-1])
+# loop to get good tuning
+while accept < 0.2 || accept > 0.3
+    accept < 0.2 ? δ *= 0.75 : nothing
+    accept > 0.3 ? δ *= 1.5 : nothing
+    chain = mcmc(θnn, θnn, δ, Σₚ, S, net, dgp; chainlength=500)
+    accept = mean(chain[:,end-1])
+end
+# final long chain
+chain = mcmc(θnn, θnn, δ, Σₚ, S, net, dgp; chainlength=5000)
+
+
