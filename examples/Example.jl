6e73e1400c5174cba6760a03b08380767a834470
Michael Creel <michael.creel@uab.es>
2024-01-08 07:18:51 +0100

    ready to test on gpu

--- examples/Example.jl
+++ examples/Example.jl
@@ -19,13 +19,13 @@ function make_net(dev)
         validation_size=2_000, loss=rmse_conv, 
         print_every=10, nsamples=100, epochs=1, dev=dev, batchsize=128
     )
-    # Create the moment network
+    # Create the net
     net = MomentNetwork(
         tcn |> hp.dev, ADAMW(), hp
     )
 end
 
-# Create and train the moment network
+# Create and train the net, and save final state
 function train_net(dgp)
     net = make_net(cpu)
     train_network!(net, dgp)
@@ -33,7 +33,7 @@ function train_net(dgp)
     BSON.@save "net_state.bson"  net_state
 end
 
-# Use trained net on CPU to do Bayesian MSM
+# Recreate trained net on CPU to do Bayesian MSM
 function load_trained()
     isfile("net_state.bson") ? @info("loading trained net") : @info("load_trained: can't create net on CPU: net_state has not been saved")
     net = make_net(cpu) # recreate the net
@@ -55,8 +55,8 @@ data, θtrue = generate(dgp, net, 1)
 # set up proposal
 covreps = 1000
 _,Σₚ = simmomentscov(net, dgp, covreps, θnn)
-δ = 2.0 # tuning
+δ = 1.0 # tuning
 
 # do MCMC
-S = 50
+S = 50 
 chain = mcmc(θnn, θnn, δ, Σₚ, S, net, dgp) 
